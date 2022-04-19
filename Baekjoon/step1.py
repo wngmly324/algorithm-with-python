69ec46d66573b3a236cc9497a288a70a3ee3fee1
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-04-19 14:45:18 +0900

    입출력과 사칙연산

---
+++ Baekjoon/step1.py
@@ -0,0 +1,72 @@
+# 2557
+print("Hello World!")
+
+# 10718
+print("강한친구 대한육군")
+print("강한친구 대한육군")
+
+# 10171
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+
+# 10172
+print("|\\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print("||_/=\\\\__|")
+
+# 1000
+a, b = map(int, input().split(" "))
+print(a+b)
+
+# 1001
+a, b = map(int, input().split(" "))
+print(a-b)
+
+# 10998
+a, b = map(int, input().split(" "))
+print(a*b)
+
+# 1008
+a, b = map(int, input().split(" "))
+print(a/b)
+
+# 10869
+a, b = map(int, input().split(" "))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+# 10926
+myid = input()
+print(myid+"??!")
+
+# 18108
+year = input()
+print(int(year) - 543)
+
+# 10430
+a, b, c = map(int, input().split(" "))
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+
+# 2588
+a = input()
+b = input()
+bList = list(b)
+
+res1 = int(a) * int(bList[2])
+res2 = int(a) * int(bList[1])
+res3 = int(a) * int(bList[0])
+
+print(res1)
+print(res2)
+print(res3)
+print(res1+res2*10+res3*100)
