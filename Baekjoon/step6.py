794b409de7972420fb732a51a80f9870c92f1e0b
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-05-05 22:47:32 +0900

    문자열6

--- Baekjoon/step6.py
+++ Baekjoon/step6.py
@@ -136,3 +136,19 @@ for j in y:
 
 aLen = len(a)
 print(aLen - sum)
+
+# 1316
+n = int(input())
+cnt = n
+
+while n:
+    a = input()
+    for j in range(len(a)-1):
+        if a[j] == a[j+1]:
+            pass
+        elif a[j] in a[j+1:]:
+            cnt -= 1
+            break
+    n -= 1
+    
+print(cnt)
