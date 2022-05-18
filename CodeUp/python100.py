047e41d01d761ae4e6aa994089382713b4d300b7
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-05-18 09:39:49 +0900

    기초-입출력

---
+++ CodeUp/python100.py
@@ -0,0 +1,108 @@
+'''
+# 6001
+print("Hello")
+
+# 6002
+print("Hello World")
+
+# 6003
+print("Hello")
+print("World")
+
+# 6004
+print("'Hello'")
+
+# 6005
+print('"Hello World"')
+
+# 6006
+print("\"!@#$%^&*()'")
+
+# 6007
+print("\"C:\Download\\'hello'.py\"")
+
+# 6008
+print('print("Hello\\nWorld")')
+
+# 6009
+x = input()
+print(x)
+
+# 6010
+x = input()
+x = int(x)
+print(x)
+
+# 6011
+f = input()
+f = float(f)
+print(f)
+
+# 6012
+a = input()
+b = input()
+a = int(a)
+b = int(b)
+print(a)
+print(b)
+
+# 6013
+a = input()
+b = input()
+print(b)
+print(a)
+
+# 6014
+f = input()
+f = float(f)
+print(f)
+print(f)
+print(f)
+
+# 6015
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a)
+print(b)
+
+# 6016
+c1, c2 = input().split()
+print(c2, c1)
+
+# 6017
+s = input()
+print(s, s, s)
+
+# 6018
+a, b = input().split(':')
+print(a, b, sep=':')
+
+# 6019
+y, m ,d = input().split('.')
+print(d, m, y, sep="-")
+
+# 6020
+bir, num = input().split('-')
+print(bir, num, sep='')
+
+# 6021
+s = input()
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+
+# 6022
+s = input()
+print(s[0:2], s[2:4], s[4:6])
+
+# 6023
+h, m, s = input().split(':')
+print(m)
+'''
+# 6024
+w1, w2 = input().split()
+s = w1 + w2
+print(s)
