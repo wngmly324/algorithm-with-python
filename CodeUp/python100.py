db11e5e24e4808e1ac7e53ac10a393a71710050b
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-05-24 15:32:24 +0900

    기초-조건/선택실행구조

--- CodeUp/python100.py
+++ CodeUp/python100.py
@@ -324,4 +324,89 @@ b = int(b)
 c = int(c)
 d = ((a if a<b else b) if ((a if a<b else b)<c) else c)
 print(int(d))
+
+# 6065
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+
+if a % 2 == 0:
+    print(a)
+    
+if b % 2 == 0:
+    print(b)
+    
+if c % 2 == 0:
+    print(c)
+
+# 6066
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+
+if a % 2 == 0:
+    print("even")
+else:
+    print("odd")
+    
+if b % 2 == 0:
+    print("even")
+else:
+    print("odd")
+    
+if c % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+# 6067
+a = int(input())
+if a < 0:
+    if a % 2 == 0:
+        print("A")
+    else:
+        print("B")
+else:
+    if a % 2 == 0:
+        print("C")
+    else:
+        print("D")
+
+# 6068
+n = int(input())
+if n >= 90:
+    print("A")
+elif n >= 70:
+    print("B")
+elif n >= 40:
+    print("C")
+else:
+    print("D")
+
+# 6069
+a = input()
+if a == "A":
+    print("best!!!")
+elif a == "B":
+    print("good!!")
+elif a == "C":
+    print("run!")
+elif a == "D":
+    print("slowly~")
+else:
+    print("what?")
+
+# 6070
+n = int(input())
+if n // 3 == 1:
+    print("spring")
+elif n // 3 == 2:
+    print("summer")
+elif n // 3 == 3:
+    print("fall")
+else:
+    print("winter")
 '''
+
