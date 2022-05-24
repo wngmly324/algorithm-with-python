ec15908cfda076f1b035755f5b84f62d0380e332
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-05-24 15:13:20 +0900

    기초-3항연산

--- CodeUp/python100.py
+++ CodeUp/python100.py
@@ -309,5 +309,19 @@ a, b = input().split()
 a = int(a)
 b = int(b)
 print(a ^ b)
-'''
 
+# 6063
+a, b = input().split()
+a = int(a)
+b = int(b)
+c = (a if a>b else b)
+print(int(c))
+'
+# 6064
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+d = ((a if a<b else b) if ((a if a<b else b)<c) else c)
+print(int(d))
+'''
