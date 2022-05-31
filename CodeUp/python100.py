3f6860aeb9f4156e298fae3b05bdc972b3a5401e
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-05-31 16:21:55 +0900

    기초-종합

--- CodeUp/python100.py
+++ CodeUp/python100.py
@@ -569,4 +569,11 @@ while n-1:
     a = a * m + d
     
 print(a)
+
+# 6091
+a, b, c = map(int, input().split())
+i = 1
+while not(i % a == 0 and i% b == 0 and i % c == 0):
+    i += 1
+print(i)
 '''
